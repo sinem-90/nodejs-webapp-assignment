316f5b56a9b09ecf2774f1da8a4b2e5cc6b6ab55
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2025-05-24 20:32:28 -0400

    update js file

--- helloworld.js
+++ helloworld.js
@@ -1,15 +1,9 @@
-var http = require("http")
+var http = require('http');
 
-http.createServer(function (request, response) {
+http.createServer(function (req, res) {
+    console.log('request received');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Welcome to Sinem\'s Amazing Web App!\n');
+}).listen(3333, '0.0.0.0');
 
-   // Send the HTTP header
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'})
-
-   // Send the response body as "Hello World"
-   res.end('Welcome to Sinem\'s Amazing Web App!\n');
-}).listen(3333)
-
-// Console will print the message
-console.log('Server running')
+console.log('Server running at http://0.0.0.0:3333/');

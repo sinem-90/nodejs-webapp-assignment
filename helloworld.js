adfc6ef3fca9e77328e0b26e0721bc320a3acd97
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2025-05-24 20:11:40 -0400

    updated name

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   res.end('Welcome to [Your Name]\'s Amazing Web App!\n');
+   res.end('Welcome to Sinem\'s Amazing Web App!\n');
 }).listen(3333)
 
 // Console will print the message

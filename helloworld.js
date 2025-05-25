f01da0805acfa604ba5bdb4eb821aedc9482af4d
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2025-05-24 20:57:04 -0400

    trying to bind at all interfaces

--- helloworld.js
+++ helloworld.js
@@ -4,6 +4,7 @@ http.createServer(function (req, res) {
     console.log('request received');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Welcome to Sinem\'s Amazing Web App!\n');
-}).listen(3333, '34.238.118.133');
+}).listen(3333); // bind to all interfaces (0.0.0.0)
+
 
 console.log('Server running at http://34.238.118.133:3333/');

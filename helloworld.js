3319d779b8ebd94674d9c49f984676dd26bbc501
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2025-05-24 20:37:32 -0400

    updated ip again

--- helloworld.js
+++ helloworld.js
@@ -4,6 +4,6 @@ http.createServer(function (req, res) {
     console.log('request received');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Welcome to Sinem\'s Amazing Web App!\n');
-}).listen(3333, '0.0.0.0');
+}).listen(3333, '34.238.118.133');
 
 console.log('Server running at http://34.238.118.133:3333/');

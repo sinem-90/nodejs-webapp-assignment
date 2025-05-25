c246a6e98faa8099bc29c0902089fb833d6efbcc
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2025-05-24 20:08:34 -0400

    initial commit

---
+++ helloworld.js
@@ -0,0 +1,15 @@
+var http = require("http")
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'})
+
+   // Send the response body as "Hello World"
+   res.end('Welcome to [Your Name]\'s Amazing Web App!\n');
+}).listen(3333)
+
+// Console will print the message
+console.log('Server running')
